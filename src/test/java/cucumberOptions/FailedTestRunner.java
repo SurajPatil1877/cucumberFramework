856e9ff7d43d41773447d8a203b602a7aa7300fa
suraj1607 <suraj1877patil@hotmail.com>
2025-04-18 14:38:09 +0530

    rerun failed test case

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -5,16 +5,14 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "@target/failed_scenarios.txt",
         glue = "stepDefinitions",
         monochrome = true,
-        tags = "@PlaceOrder or @OffersPage",
         plugin = {"pretty",
                 "html:target/cucumber.html",
                 "json:target/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "rerun:target/failed_scenarios.txt"})
-public class TestNgRunner extends AbstractTestNGCucumberTests {
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
