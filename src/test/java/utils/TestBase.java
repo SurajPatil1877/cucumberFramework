e95b12dcfb9c3bdeb89a73d2ede06a516ee1fc18
suraj <suraj777patil@gmail.com>
2025-06-08 20:06:23 +0530

    1. add chrome driver for linux 2. update TestBase class 3. update chrome driver location

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -3,26 +3,45 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Properties;
 
 public class TestBase {
     private WebDriver driver;
+    private final Path propFilePath;
+    private final String driverPath;
+
+    public TestBase() {
+        String os = System.getProperty("os.name").toLowerCase();
+        Path basePath = Paths.get(System.getProperty("user.dir"), "src", "test", "resources");
+
+        propFilePath = basePath.resolve(Paths.get("properties", "global.properties"));
+        driverPath = basePath
+                .resolve(Paths.get("driver", os.contains("linux") ? "linux" : "windows", os.contains("linux") ? "chromedriver" : "chromedriver.exe"))
+                .toAbsolutePath()
+                .toString();
+    }
 
     public WebDriver webDriverManager() {
         if (driver == null) {
             Properties prop = new Properties();
-            String baseLocation = System.getProperty("user.dir") + "\\src\\test\\resources\\";
-            try {
-                prop.load(new FileInputStream(baseLocation + "properties\\global.properties"));
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+
+            try (InputStream input = java.nio.file.Files.newInputStream(propFilePath)) {
+                prop.load(input);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load properties file: " + propFilePath, e);
             }
 
-            if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver", baseLocation + "driver\\chromedriver.exe");
+            String browser = prop.getProperty("browser", "chrome").toLowerCase();
+            if (browser.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", driverPath);
                 driver = new ChromeDriver();
+            } else {
+                throw new UnsupportedOperationException("Browser not supported: " + browser);
             }
 
             driver.manage().window().maximize();
